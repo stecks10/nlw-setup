fa68dd386bb87a47a07d20aadfcae16dca9efa29
Vitor Nunes <stecks10@gmail.com>
2023-01-24 18:41:04 -0300

    ajustes no mobile aula-3

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,6 +1,7 @@
-import { ScrollView, View, Text } from "react-native";
 import { useRoute } from "@react-navigation/native";
+import { ScrollView, Text, View } from "react-native";
 import dayjs from "dayjs";
+
 import { BackButton } from "../components/BackButton";
 import { ProgressBar } from "../components/ProgressBar";
 import { CheckBox } from "../components/CheckBox";
@@ -19,23 +20,34 @@ export function Habit() {
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 100 }}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
         <BackButton />
 
         <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
           {dayOfWeek}
         </Text>
+
         <Text className="text-white font-extrabold text-3xl">
           {dayAndMonth}
         </Text>
+
         <ProgressBar progress={30} />
+
         <View className="mt-6">
-          <CheckBox title="Beber 2l agua" checked={false} />
-          <CheckBox title="Caminhar" checked={true} />
+          <CheckBox
+            title="Beber 2L de água"
+            checked={false}
+          />
+
+          <CheckBox
+            title="Caminhar"
+            checked
+          />
         </View>
       </ScrollView>
-
     </View>
   )
 }
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -9,22 +9,22 @@ import { useNavigation } from "@react-navigation/native";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const datesFromYearStart = generateRangeDatesFromYearStart();
-const minimumSummaryDatesSizes = 18 * 5;
-const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
+const minimunSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length
 
 export function Home() {
-  const { navigate } = useNavigation();
-
+  const { navigate } = useNavigation()
   return (
-    <View className="flex-1 bg-background px-8 pt-16">
+    <View className='flex-1 bg-background px-8 pt-16'>
       <Header />
 
       <View className="flex-row mt-6 mb-2">
         {
           weekDays.map((weekDay, i) => (
-            <Text key={`${weekDay}-${i}`}
+            <Text
+              key={`${weekDay}-${i}`}
               className="text-zinc-400 text-xl font-bold text-center mx-1"
-              style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              style={{ width: DAY_SIZE }}
             >
               {weekDay}
             </Text>
@@ -32,22 +32,27 @@ export function Home() {
         }
       </View>
 
-      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 100 }}>
-        <View className="flex-row flex-wrap">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className='flex-row flex-wrap'>
           {
-            datesFromYearStart.map((date, i) => (
+            datesFromYearStart.map(date => (
               <HabitDay
                 key={date.toISOString()}
-                onPress={() => navigate('new')}
+                onPress={() => navigate('habit', { date: date.toISOString() })}
               />
+
             ))
           }
+
           {
             amountOfDaysToFill > 0 && Array
               .from({ length: amountOfDaysToFill })
-              .map((_, i) => (
+              .map((_, index) => (
                 <View
-                  key={i}
+                  key={index}
                   className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
                   style={{ width: DAY_SIZE, height: DAY_SIZE }}
                 />
@@ -56,7 +61,6 @@ export function Home() {
         </View>
       </ScrollView>
 
-
     </View>
   )
-}
+} 
